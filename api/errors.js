b7a22fac7e5e6934bdf6dc4dc951e978542a9de5
irajyus <irach.yusufiy@getnacelle.com>
2022-11-03 01:07:52 -0400

    remove console logs - add auth console log

--- api/errors.js
+++ api/errors.js
@@ -9,20 +9,17 @@ const router = express.Router();
 router.post("/", (req, res) => {
   console.log(req.body);
   const production = req.body.payload.target === "production";
-  console.log(production);
   const payload = JSON.stringify(req.body);
-  console.log(typeof payload);
   const xvs = req.get("x-vercel-signature");
   const signature = crypto
     .createHmac("sha1", process.env.OAUTH2_SECRET)
     .update(payload)
     .digest("hex");
+  console.log(xvs, signature);
   const { name, inspectorUrl } = req.body.payload.deployment;
   const event = { name, inspectorUrl };
   if (signature === xvs && production) {
     Slack.sendDeployErrorNotification(event);
-    // console.log(payload);
-    // console.log(xvs);
     return res.json({
       success: true,
       event,
