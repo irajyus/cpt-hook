8cb49b6dd0c35e900677def86227b11d129a80c8
irajyus <irach.yusufiy@getnacelle.com>
2022-11-03 02:43:32 -0400

    fixes

--- index.js
+++ index.js
@@ -6,8 +6,4 @@ import errors from "./api/errors";
 
 app.use(json({ extended: false }));
 app.use("/api/errors", errors);
-
-const port = 5000;
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
-});
+app.listen("api/errors");
