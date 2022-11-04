624d98b94c3b6806302d5424b301668b39eba815
irajyus <irach.yusufiy@getnacelle.com>
2022-11-04 00:03:19 -0400

    fix

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ import errors from "./api/errors";
 import bodyParser from "body-parser";
 const app = express();
 const bodyParser = require("body-parser");
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use("/api/errors", errors);
 app.listen("/api/errors");
